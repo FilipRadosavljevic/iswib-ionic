bf7d2e9609d8a96ce8b85f4b1b89917c2691f35c
Filip Radosavljevic <fradosavljevic98@gmail.com>
2022-05-08 14:05:16 +0200

    Complete design and segment for home page added json for schedule

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
+
+  type: string;
+  data: any;
 
   constructor() {}
+  // constructor(public http: HttpClient) {}
 
+  ngOnInit() {
+    this.type = 'day1';
+    this.getData();
+  }
+
+  segmentChanged(ev: any) {
+    console.log('Segment changed', ev);
+  }
+
+  getData() {
+    fetch('../assets/data/schedule.json').then(res => res.json())
+    .then(json => {
+      this.data = json;
+      console.table(this.data);
+    });
+  }
 }
