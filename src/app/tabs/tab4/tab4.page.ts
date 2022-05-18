307170197b062c618c77e9e5f2d7088029d233c0
Filip Radosavljevic <fradosavljevic98@gmail.com>
2022-05-18 23:10:16 +0200

    design finished for city discovery

--- src/app/tabs/tab4/tab4.page.ts
+++ src/app/tabs/tab4/tab4.page.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab4',
   templateUrl: 'tab4.page.html',
   styleUrls: ['tab4.page.scss']
 })
-export class Tab4Page {
+export class Tab4Page implements OnInit{
 
-  constructor() {}
+  data: any;
+  likes: number;
+  dislikes: number;
+
+  constructor() {
+    this.likes = 0;
+    this.dislikes = 0;
+  }
+
+  ngOnInit() {
+    this.getData();
+  }
+
+  async getData() {
+    await fetch('../assets/data/discovery.json').then(res => res.json())
+    .then(json => {
+      this.data = json;
+      console.log(this.data);
+      console.table(this.data);
+    });
+  }
+
+  handleLike() {
+    this.likes++;
+  }
+
+  handleDislike() {
+    this.dislikes++;
+  }
+
+  handleArrow(event) {
+    console.log(event);
+  }
 
 }
