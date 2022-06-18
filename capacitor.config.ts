12eaa110a339f8cf117375d9323a8cd433206591
Filip Radosavljevic <filip.radosavljevic@united.cloud>
2022-06-18 19:30:25 +0200

    iOS builded, camera deleted

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.iswib.app',
   appName: 'ISWiB',
   webDir: 'www',
   bundledWebRuntime: false
