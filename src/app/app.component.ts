140130b09e4e6a8e47a6b428183766eb4f720cb1
Filip Radosavljevic <fradosavljevic98@gmail.com>
2022-06-22 19:02:01 +0200

    Styling and animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
 
+import { AnimationController } from '@ionic/angular';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
+  styleUrls: ['app.component.scss']
 })
 export class AppComponent {
-  constructor() {}
 
+  constructor(
+    private animationCtrl: AnimationController
+  ) {}
+
+
+  myCustomPageTransition = ((baseEl: any, opts?: any) => {
+      console.log('opts.enteringEl:'  + opts.enteringEl); //Entering Element - New Page
+      console.log('opts.leavingEl:'  + opts.leavingEl);   //Leaving Element - Current Page
+      const anim1 = this.animationCtrl.create()
+        .addElement(opts.leavingEl)
+        .duration(600)
+        .iterations(1)
+        .easing('ease-out')
+        .fromTo('transform', 'translateX(0px)', 'translateX(100%)')
+        .fromTo('opacity', '1', '0.2');
+      let anim2 = this.animationCtrl.create()
+        .addElement(opts.enteringEl)
+        .duration(600)
+        .iterations(1)
+        .easing('ease-out')
+        .fromTo('opacity', '0.0', '1');
+        anim2 = this.animationCtrl.create()
+        .duration(600)
+        .iterations(1)
+        .addAnimation([anim1, anim2]);
+      return anim2;
+  });
 }
--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-// import { CameraPreviewOptions } from '@capacitor-community/camera-preview';
-// import { CameraPreview } from '@capacitor-community/camera-preview';
-
 
 @Component({
   selector: 'app-tabs',
@@ -9,22 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['tabs.page.scss']
 })
 export class TabsPage {
-  // cameraActive = false;
 
-  constructor() {
-    const tabs = document.querySelectorAll('ion-tab-button');
-    Object.keys(tabs).map((key) => {
-      tabs[key].style.display = 'none';
-    });
-    console.log('asd',tabs);
-  }
-  // openCamera() {
-  //   const cameraPreviewOptions: CameraPreviewOptions = {
-  //     position: 'rear',
-  //     width: window.screen.width-1,
-  //     height: window.screen.height-300
-  //   };
-  //   CameraPreview.start(cameraPreviewOptions);
-  //   // this.cameraActive = true;
-  // }
+  constructor() {}
 }
