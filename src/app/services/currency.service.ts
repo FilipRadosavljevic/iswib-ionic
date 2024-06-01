f1ae1b9dd3a46441e8a3a8bcf6800f9145345220
Denda Bojan <bojan.denda@stagefronttickets.com>
2024-06-01 17:26:09 +0200

    Applied Converter with search working Commit

--- src/app/services/currency.service.ts
+++ src/app/services/currency.service.ts
@@ -1,76 +1,78 @@
 /* eslint-disable no-underscore-dangle */
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { environment } from 'src/environments/environment';
-import { getDatabase, ref, set, get } from '@angular/fire/database';
-import { BehaviorSubject } from 'rxjs';
-import { map, switchMap, tap } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http'
+import { Injectable } from '@angular/core'
+import { environment } from 'src/environments/environment'
+import { switchMap, tap } from 'rxjs/operators'
+import { writeBatch, doc, Firestore, getDocs, collection } from '@angular/fire/firestore'
 
 export interface Currency {
-  name: string;
-  code: string;
-  rate: number;
+  name: string
+  code: string
+  rate: number
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CurrencyService {
-  currencies: Currency[];
+  currencies: Currency[]
 
   constructor(
-    private http: HttpClient
-    ) { }
+    private http: HttpClient,
+    private firestore: Firestore,
+  ) {}
 
   async fetchCurrencies() {
-    try{
-      const data = await get(ref(getDatabase(),'/Currencies'));
-      if(data){
-        this.currencies = data.val();
-      }
-      else{
-        console.error('No available rates data.');
-      }
-    } catch(error){
-      console.error(error);
+    try {
+      const dataSnapshot = await getDocs(collection(this.firestore, 'currencies'))
+      const currencies: Currency[] = []
+      dataSnapshot.forEach((document) => {
+        //console.log(document.data());
+        currencies.push(document.data() as Currency)
+      })
+      return currencies
+    } catch (error) {
+      console.error(error)
+      return []
     }
   }
 
-  dowloadNewRates() {
-    let currencies: Currency[];
-    this.http.get<any>('https://api.apilayer.com/exchangerates_data/symbols',
-    { headers: { apikey: environment.currencyAPI} })
-    .pipe(
-      switchMap(resData => {
-        //let currencies: Currency[];
-        console.log(resData);
-        for (const key in resData.symbols) {
-          if (resData.symbols.hasOwnProperty(key)) {
-            console.log(typeof currencies);
-            currencies.push(
-              {
-                code: key,
-                name: resData.symbols[key],
-                rate: null
-              }
-            );
-          }
-        }
-        return this.http.get<any>('https://api.apilayer.com/exchangerates_data/latest',
-        { headers: { apikey: environment.currencyAPI} });
-      }),
-      tap(resData=>{
-        console.log(resData);
-        for (const key in resData.rates) {
-          if (resData.rates.hasOwnProperty(key)) {
-            const curr = currencies.find((value)=>value.code===key);
-            curr.rate = resData.rates[key];
-          }
-        }
-        this.currencies = currencies;
-        set(ref(getDatabase(), '/Currencies'), currencies);
-      }),
-    ).subscribe();
-  }
-
+  // downloadNewRates() {
+  //   const currencies: Currency[] = [];
+  //   this.http.get<any>('https://api.apilayer.com/exchangerates_data/symbols',
+  //   { headers: { apikey: environment.currencyAPI} })
+  //   .pipe(
+  //     switchMap(resData => {
+  //       //let currencies: Currency[];
+  //       console.log(resData);
+  //       for (const key in resData.symbols) {
+  //         if (resData.symbols.hasOwnProperty(key)) {
+  //           console.log(typeof currencies);
+  //           currencies.push(
+  //             {
+  //               code: key,
+  //               name: resData.symbols[key],
+  //               rate: null
+  //             }
+  //           );
+  //         }
+  //       }
+  //       return this.http.get<any>('https://api.apilayer.com/exchangerates_data/latest',
+  //       { headers: { apikey: environment.currencyAPI} });
+  //     }),
+  //     tap(async resData=>{
+  //       const batch = writeBatch(this.firestore);
+  //       console.log(resData);
+  //       for (const key in resData.rates) {
+  //         if (resData.rates.hasOwnProperty(key)) {
+  //           const curr = currencies.find((value)=>value.code===key);
+  //           curr.rate = resData.rates[key];
+  //           batch.set(doc(this.firestore,'currencies',curr.code),curr);
+  //         }
+  //       }
+  //       this.currencies = currencies;
+  //       await batch.commit();
+  //     }),
+  //   ).subscribe();
+  // }
 }
