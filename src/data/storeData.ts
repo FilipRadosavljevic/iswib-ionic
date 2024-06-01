44d5c89fa7576ffe89974991bc49047420055ae5
Denda Bojan <bojan.denda@stagefronttickets.com>
2024-06-01 14:02:37 +0200

    WIP

---
+++ src/data/storeData.ts
@@ -0,0 +1,29 @@
+import { Product } from 'src/app/models/product.model';
+
+export const companyStoreProducts: Product[] = [
+  {
+    name: 'Backpack',
+    price: 8,
+    image: '/assets/images/store/Ranac.jpeg'
+  },
+  {
+    name: 'Magnet 1',
+    price: 2,
+    image: '/assets/images/store/Magnet_1.jpg'
+  },
+  {
+    name: 'Magnet 2',
+    price: 2,
+    image: '/assets/images/store/Magnet_2.jpg'
+  },
+  {
+    name: 'Socks',
+    price: 4,
+    image: '/assets/images/store/Carape.png'
+  },
+  {
+    name: 'Notebook',
+    price: 6,
+    image: '/assets/images/store/Rokovnik.jpeg'
+  },
+];
